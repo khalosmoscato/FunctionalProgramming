ad7672dd671d985227fe6ee334accdc7763b0e53
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-27 14:54:04 +0000

    feat: task 7 -> add Exercises002 class with a GetCoolPeople method + tests

---
+++ FunctionalProgramming.Tasks/Exercises002.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalProgramming.Tasks
+{
+    public class Exercises002
+    {
+        public static Func<List<string>> GetCoolPeople = () => new List<string>
+        {
+            "rich.neat@boardgamer.com",
+            "poppy.mcdonnell@irishdancer.com",
+            "neil.hughes@walkingoncustard.com",
+            "alice.yang@midfielder.com",
+            "pippa.austin@musician.com"
+        };
+    }
+}
---
+++ FunctionalProgramming.Tests/Exercises002Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using FunctionalProgramming.Tasks;
+
+namespace FunctionalProgramming.Tests
+{
+    public class Exercises002Tests
+    {
+        [Fact]
+        public void GetCoolPeople_ReturnsAListOfUserEmails()
+        {
+            List<string> test = new List<string>
+        {
+            "rich.neat@boardgamer.com",
+            "poppy.mcdonnell@irishdancer.com",
+            "neil.hughes@walkingoncustard.com",
+            "alice.yang@midfielder.com",
+            "pippa.austin@musician.com"
+        };
+            
+            List<string> result = Exercises002.GetCoolPeople();
+
+            result.Should().BeEquivalentTo(test);
+        }
+    }
+}
