1b551713d83d3db68aa5c4a485d5c3a38d302aae
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-27 13:17:48 +0000

    feat: task 1 & 2 -> add and test class Exercises001

--- FunctionalProgramming.Tests/Exercises001Tests.cs
+++ FunctionalProgramming.Tests/Exercises001Tests.cs
@@ -1,10 +1,18 @@
-﻿namespace FunctionalProgramming.Tests;
+﻿using FluentAssertions;
+using FunctionalProgramming.Tasks;
+
+namespace FunctionalProgramming.Tests;
 
 public class Exercises001Tests
 {
     [Fact]
-    public void Test1()
+    public void SquareIt_ReturnsSquaredArg()
     {
+        int input = 10;
+        int expected = 100;
 
+        int result = Exercises001.SquareIt(10);
+
+        result.Should().Be(expected);
     }
 }
