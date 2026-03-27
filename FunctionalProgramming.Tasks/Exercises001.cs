ac0e021fc9a438d8e6ea2cc8106ea3c2d43d1d33
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-27 13:36:54 +0000

    feat: task 3 -> add and test AddTen Lambda

--- FunctionalProgramming.Tasks/Exercises001.cs
+++ FunctionalProgramming.Tasks/Exercises001.cs
@@ -7,6 +7,7 @@ namespace FunctionalProgramming.Tasks
 {
     public class Exercises001
     {
-        public static Func<int, int> SquareIt = (x) => x * x;
+        public static Func<int, int> SquareIt = x => x * x;
+        public static Func<int, int> AddTen = x => x + 10;
     }
 }
--- FunctionalProgramming.Tests/Exercises001Tests.cs
+++ FunctionalProgramming.Tests/Exercises001Tests.cs
@@ -11,8 +11,24 @@ public class Exercises001Tests
         int input = 10;
         int expected = 100;
 
-        int result = Exercises001.SquareIt(10);
+        int result = Exercises001.SquareIt(input);
 
         result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void AddTen_ReturnsNumberPlusTen()
+    {
+        List<int> input = new List<int> { 4, 15, 55, 78, 12 };
+        List<int> expected = new List<int> { 14, 25, 65, 88, 22 };
+        List<int> result = new();
+
+        foreach (int i in input)
+        {
+            int n = Exercises001.AddTen(i);
+            result.Add(n);
+        }
+        
+        result.Should().Equal(expected);
     }
 }
