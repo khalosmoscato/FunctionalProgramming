af3d133fe104d7155c9fbb2b2f309a5504275c13
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-27 13:08:51 +0000

    Add project files.

---
+++ FunctionalProgramming.Tasks/Exercises001.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FunctionalProgramming.Tasks
+{
+    public class Exercises001
+    {
+        public static Func<int, int> SquareIt = (x) => x * x;
+    }
+}
---
+++ FunctionalProgramming.Tests/Exercises001Tests.cs
@@ -0,0 +1,10 @@
+﻿namespace FunctionalProgramming.Tests;
+
+public class Exercises001Tests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
